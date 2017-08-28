e0d25892a1204370facbd31161bc3ea88cc2b7c3
rizzyc <rizzyc@gmail.com>
2017-08-28 16:39:21 -0400

    Update AlbumDetail.js
    A bulk of the changes occur in this files, and the functional components just spit out some views

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,65 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, Image, Linking } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => (
+const AlbumDetail = ({ album }) => {
+    //destructuring... so don't have to repeat props.blah each time
+    const { title, artist, thumbnail_image, image, url } = album;
+
+    return (
     <Card>
-        <Text>{props.album.title}</Text>
+        <CardSection>
+            <View style={styles.thumbnailContainerStyle}>
+                <Image 
+                style={styles.thumbnailStyle}
+                source={{ uri: thumbnail_image }} 
+                />
+            </View>
+            <View style={styles.headerContentStyle}>
+                <Text style={styles.headerTextStyle}>{title}</Text>
+                <Text>{artist}</Text>
+            </View>
+        </CardSection>
+        <CardSection>
+            <Image
+            style={styles.imageStyle}
+            source={{ uri: image }}
+            />
+        </CardSection>
+        <CardSection>
+            <Button onPress={() => Linking.openURL(url)}>
+                Buy Now
+            </Button>
+        </CardSection>
     </Card>
 );
+};
 
+const styles = StyleSheet.create({
+   headerContentStyle: {
+       flexDirection: 'column',
+        justifyContent: 'space-around'
+   },
+   headerTextStyle: {
+    fontSize: 18
+   },
+   thumbnailStyle: {
+       height: 50,
+       width: 50
+   },
+   thumbnailContainerStyle: {
+       justifyContent: 'center',
+       alignItems: 'center',
+       marginLeft: 10,
+       marginRight: 10
+   },   
+   imageStyle: {
+       height: 300,
+       flex: 1,
+       width: null
+   }
+});
 
 export default AlbumDetail;
