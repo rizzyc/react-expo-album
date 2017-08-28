18ae41a34973695ade2e3995d3e154c037b2de69
rizzyc <rizzyc@gmail.com>
2017-08-28 16:40:22 -0400

    Update AlbumList.js
    The only major change here was adding ScrollView adding this where the album list renders makes the App Header sticky at the top.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import AlbumDetail from './AlbumDetail';
 
 export default class AlbumList extends React.Component {
     state = { albums: [] };
 
     componentWillMount() {
-       fetch('http://rallycoding.herokuapp.com/api/music_albums')
-       .then(response => response.json())
-       .then(data => this.setState({ albums: data }));
+        fetch('http://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => response.json())
+        .then(data => this.setState({ albums: data }));
     }
 
     renderAlbums() {
-        return this.state.albums.map(album => 
+        return this.state.albums.map(album =>
         <AlbumDetail key={album.title} album={album} />);
     }
+
     render() {
-        //console.log(this.state);
         return (
-        <View>
-            {this.renderAlbums()}
-        </View>
+            <ScrollView>
+                { this.renderAlbums() }
+            </ScrollView>
         );
     }
 }
-
